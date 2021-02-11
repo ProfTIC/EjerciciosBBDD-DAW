bba909db0e362ca93e3e668f1d13d6b402284ebf
Prof.TIC <prof.tic.informatica@gmail.com>
2021-02-11 20:28:41 +0100

    Añadido enunciado actividad 6.1

---
+++ Tema6/Actividad6-1.sql
@@ -0,0 +1,35 @@
+-- Soluciones Actividad 6.1 
+
+--  1. Muestra todos los datos de las asignaturas
+   
+--  2. Muestra la descripción y el departamento de todas las asignaturas
+   
+--  3. Muestra el código de asignatura y la fecha de todas las matrículas. La información debe aparecer tal y como se muestra a continuación, apareciendo antes las más recientes y con cuatro dígitos para el año
+       
+--  4. ¿Qué asignaturas no pertenecen a ningún departamento?
+   
+--  5. Obtén un listado de las matrículas donde figure la descripción de cada asignatura ordenados por nota de forma ascendente
+   
+--  6. Muestra el nombre y los apellidos de todos los alumnos cuyo código de localidad sea 14049. Ten en cuenta que el nombre de las columnas debe aparecer tal y como se muestra a continuación
+   
+--  7. Muestra los datos de todas las asignaturas de 2-ESO, 3-ESO y 4-ESO
+   
+--  8. Muestra los datos de todas las asignaturas cuyo código esté entre 3 y 6
+   
+--  9. Obtén un listado de todos los alumnos, uniendo en una sola columna el nombre y los dos apellidos
+   
+-- 10. Obtén un listado con todas las asignaturas que tengan algún departamento asignado
+  
+--  11. Selecciona todos los alumnos cuyo primer apellido termine en 'a'
+       
+--  12. Selecciona todos los alumnos cuyo primer apellido empiece por ‘M’ y termine en 'a'
+       
+-- 13. Selecciona todos los alumnos cuyo primer apellido termine en 'a' y tengan 9 caracteres
+       
+--  14. Devuelve los datos de los dos primeros alumnos
+  
+--  15. Obtén un listado de todas las asignaturas ordenadas por descripción en orden ascendente y por curso en orden descendente
+  
+--  16. Devuelve los datos de las tres asignaturas a partir de la segunda
+       
+       
