a211736d4aeb9ce1c79acc7c30ead430ec86ce85
Prof.TIC <prof.tic.informatica@gmail.com>
2021-02-18 19:42:27 +0100

    Enunciados Actividad 6.2

---
+++ Tema6/Actividad6-2/Actividad6-2.sql
@@ -0,0 +1,40 @@
+-- 1. Muestra el precio con IVA para todos los productos
+
+-- 2. Muestra el precio con IVA para los productos que valgan menos de 11 euros
+              
+-- 3. Muestra la denominación y el precio con IVA para los productos cuyo precio esté entre 10 y 20 euros
+
+-- 4. Muestra todas las secciones en los que el número del empleado jefe sea menor que 5
+
+-- 5. Muestra los datos del empleado cuyo código sea la raíz cuadrada de 4
+
+-- 6. Obtén las iniciales de los empleados
+
+-- 7. Selecciona el nombre y la longitud del mismo para todos los empleados
+
+-- 8. Muestra el nombre completo de todos los empleados (nombre + apellido1+ apellido2)
+
+-- 9. Realiza la misma consulta que en el apartado 7 pero el valor devuelto deberá estar en minúsculas
+
+-- 10. Realiza la misma consulta que en el apartado, mostrando el nombre como esté almacenado en la base de datos y los dos apellidos en mayúscula. Por ejemplo, Santiago RODRÍGUEZ RODRÍGUEZ
+
+-- 11. Muestra el código de la provincia y el nombre separados por un guión. Usa concat_ws. 
+
+-- 12. Muestra el código de la provincia y el nombre con el siguiente formato: Albacete tiene el código de provincia 01
+
+-- 13. Muestra todas las provincias cuyo nombre tenga diez o más caracteres
+
+-- 14. Muestra los empleados que sean de Granada (el código es 17)
+
+-- 15. Muestra todos los empleados de la sección EDU
+
+-- 16. Muestra el nombre y el correo de las empresas que tengan un correo acabado en .es
+
+-- 17. Muestra el código, el nombre y el teléfono de las empresas cuyo nombre empiece por Edu
+
+-- 18. Muestra los caracteres 2 a 6 de la descripción de todas las secciones
+
+-- 19. Muestra los primeros cinco caracteres del nombre de las empresas
+
+-- 20. Muestra la parte izquierda del correo de todas las empresas (lo que aparece delante de @)
+
